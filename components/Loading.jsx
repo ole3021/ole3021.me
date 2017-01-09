15a52bf99113308968a02acf55f736535aabd008
ole3021 <ole3021@gmail.com>
2017-01-09 11:06:11 +0800

    update animejs example

--- components/Loading.jsx
+++ components/Loading.jsx
@@ -2,10 +2,23 @@ import React from 'react'
 import anime from 'animejs'
 
 class Loading extends React.Component {
+  componentDidMount () {
+    anime({
+      targets: ['.blue', '.green'],
+      translateX: '13rem',
+      rotate: 180,
+      borderRadius: '8px',
+      duration: 2000,
+      loop: true
+    })
+  }
+
   render () {
     return (
       <div>
         loading...
+        <div className='block green' />
+        <div className='block green' />
       </div>
     )
   }
