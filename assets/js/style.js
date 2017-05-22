df8eb3d7969128bbcf85add7d72a32d12eaafc35
Oliver.W <ole3021@gmail.com>
2017-05-22 19:28:58 +0800

    update color, add toggle menu

---
+++ assets/js/style.js
@@ -0,0 +1,6 @@
+// Toogle Burger Menu
+const toggleMenu = document.getElementsByClassName('toggle')
+
+toggleMenu[0].addEventListener('click', function () {
+  toggleMenu[0].classList.toggle('open')
+})
