351b50e9a6c5e0c154ae966e3bd91bbcb54607e5
Oliver.W <ole3021@gmail.com>
2017-09-11 20:51:31 +0800

    complete stule

--- pages/Blogs.jsx
+++ pages/Blogs.jsx
@@ -3,76 +3,52 @@ import { Link, browserHistory } from 'react-router'
 import classNames from 'classnames'
 import axios from 'axios'
 import yaml from 'js-yaml'
+import mixitup from 'mixitup'
 
 import Loading from '../components/Loading'
-import BlogItem from '../components/BlogItem'
 import { buildCategories } from '../utils'
 
-class CategoryTabs extends React.Component {
+window.mixitup = mixitup
+
+class CategorySwitcher extends React.Component {
   render () {
     const { props: { blogCategories, currentCategory } } = this
 
-    const CategoryNavs = blogCategories
-      ? Object.keys(blogCategories).map((name, index) => {
-        return <li key={index}
-          className={classNames('uk-visible@s', {'uk-active': currentCategory === name})}>
-          <Link to={`/blogs/${name}`}>{name.replace('_', ' ')}</Link>
-        </li>
-      })
-      : null
+    const categorySwitchers = blogCategories ? Object.keys(blogCategories).map((name, index) => {
+      return (
+        <Link key={index} to={`/blogs/${name}`} className={classNames('uk-button', {'uk-button-primary': currentCategory === name})} data-mix-filter={`.${name}`}>
+          {name.replace('_', ' ')}
+        </Link>
+      )
+    })
+    : null
 
-    const CategoryNavS = blogCategories
-      ? Object.keys(blogCategories).map((name, index) => {
-        return <li key={index}
-          className={classNames('uk-hidden@s', {'uk-active': currentCategory === name})}>
-          <Link to={`/blogs/${name}`}>{name.replace('_', ' ')}</Link>
-        </li>
-      })
-      : null
     return (
-      <ul className='uk-subnav uk-subnav-pill' data-uk-switcher>
-        {CategoryNavs}
-
-        <li className='uk-hidden@s'>
-          <a href='#'>{currentCategory}<span data-uk-icon={'icon: triangle-down'}></span></a>
-          <div data-uk-dropdown={'mode: click;'}>
-              <ul className='uk-nav uk-dropdown-nav'>
-                {CategoryNavS}
-              </ul>
+      <div className='uk-container uk-container-center'>
+        <div className='uk-flex uk-flex-center'>
+          <div className='uk-button-group uk-visible@s'>
+            {categorySwitchers}
           </div>
-        </li>
-      </ul>      
+          <div className='uk-inline uk-hidden@s'>
+            <button className='uk-button uk-button-default' type='button'>Click</button>
+            <div data-uk-dropdown={'mode: click'}>
+              {categorySwitchers}
+            </div>
+          </div>
+        </div>
+      </div>
     )
   }
 }
 
 class CategoryBlogs extends React.Component {
   render () {
-    const { props: { blogCategories, currentCategory } } = this
-
-    const blogList = (blogs) => {
-      const blogItems = blogs.map((blog, index) => 
-        <Link key={index} to={`/post/${blog.title}`} className='uk-width-1-3@s uk-width-1-4@m'>
-          <BlogItem blog={blog} />
-        </Link>
-      )
-      return (
-        <div className='uk-grid-medium uk-child-width-expand@s uk-text-center' data-uk-grid>
-          {blogItems}
-        </div>
-      )
-    }
-
-    const blogByCategories = blogCategories
-      ? Object.keys(blogCategories).map((name, index) => 
-        <li key={index}>{blogList(blogCategories[name])}</li>
-      )
-      : <Loading />
-
     return (
-      <ul className='uk-switcher uk-margin'>
-        {blogByCategories}
-      </ul>
+      <div className='uk-section'>
+        <div className='uk-container uk-container-center'>
+          <ul id='mix-wrapper' className='uk-grid-medium uk-child-width-expand@s uk-text-center' style={{}} data-uk-grid data-mix-container />
+        </div>
+      </div>
     )
   }
 }
@@ -81,35 +57,78 @@ class Blogs extends React.Component {
   constructor (props) {
     super(props)
 
+    this.mixer = null
+
     this.state = {
       isLoading: true,
-      blogCategories: null
+      blogCategories: null,
+      posts: null
     }
   }
 
   componentDidMount () {
     const { props: { params: { category } } } = this
     const postInfoPath = '/postInfos.yml'
+
+    const container = document.querySelector('[data-mix-container]')
+
+    const render = blog => (
+      `<li class='mix-target ${blog.category} uk-width-1-1@s uk-width-1-3@m uk-width-1-4@l' data-mix-item data-mix-date=${blog.created.toISOString().split('T')[0]}>` +
+        `<a href='/post/${blog.title}'>` +
+          `<div class='uk-margin uk-text-center uk-card uk-card-default uk-card-small'>` +
+            `<div class='uk-card-media-top'>` +
+              `<img src=${blog.cover} alt='' />` +
+            `</div>` +
+            `<div class='uk-card-body'>` +
+              `<h3 class='uk-card-title'>${blog.title}</h3>` +
+              `<div class='el-meta uk-margin uk-text-meta'>${blog.meta}</div>` +
+            `</div>` +
+          `</div>` +
+        `</a>` +
+      `</li>`
+    )
+
+    this.mixer = mixitup(container, {
+      data: {
+        uidKey: 'path'
+      },
+      render: {
+        target: render
+      },
+      selectors: {
+        target: '[data-mix-item]'
+      }
+    })
+
     axios.get(postInfoPath).then(({data}) => {
       const posts = yaml.load(data)
       const categories = buildCategories(posts)
-      // article = marked(data)
-      this.setState({ isLoading: false, blogCategories: categories })
+      this.setState({ isLoading: false, blogCategories: categories, posts: posts })
+
       if (!category) {
-        browserHistory.push(`/blogs/${Object.keys(categories)[0]}`)
+        browserHistory.push(`/blogs/all`)
       }
+
+      this.mixer.dataset(posts)
+      this.mixer.filter(category === 'all' ? 'all' : `.${category}`)
     })
   }
 
+  componentWillUnmount () {
+    this.mixer.destroy()
+  }
+
   render () {
-    const { props: { params: { category } }, state: { blogCategories } } = this
+    const { props: { params: { category } }, state: { blogCategories, posts } } = this
+
+    if (category && this.mixer) {
+      this.mixer.filter(category === 'all' ? 'all' : `.${category}`)
+    }
 
     return (
       <div className='uk-container'>
-        <div className='uk-flex uk-flex-column uk-flex-middle'>
-          <CategoryTabs currentCategory={category} blogCategories={blogCategories}/>
-          <CategoryBlogs currentCategory={category} blogCategories={blogCategories} />
-        </div>
+        <CategorySwitcher currentCategory={category} blogCategories={blogCategories} />
+        <CategoryBlogs currentCategory={category} posts={posts} />
       </div>
     )
   }
