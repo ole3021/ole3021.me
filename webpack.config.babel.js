2a178b3390e9899df84485cdfa1d779f3c4c7cf9
Oliver.W <ole3021@gmail.com>
2018-05-12 11:17:59 +0800

    init preact

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,60 +1,222 @@
-import webpack from 'webpack'
-import ExtractTextPlugin from 'extract-text-webpack-plugin'
+import webpack from "webpack";
+import ExtractTextPlugin from "extract-text-webpack-plugin";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import autoprefixer from "autoprefixer";
+import CopyWebpackPlugin from "copy-webpack-plugin";
+import OfflinePlugin from "offline-plugin";
+import path from "path";
+const ENV = process.env.NODE_ENV || "development";
 
-const plugins = [
-  new ExtractTextPlugin('./bundle.css'),
-  new webpack.DefinePlugin({
-    'process.env': {
-      NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development')
-    }
-  })
-]
+const CSS_MAPS = ENV !== "production";
 
-if (process.env.NODE_ENV === 'production') {
-  plugins.push(new webpack.optimize.UglifyJsPlugin({
-    compress: {
-      warnings: false
-    },
-    output: {
-      comments: false
-    }
-  }))
-}
+module.exports = {
+  context: path.resolve(__dirname, "src"),
+  entry: "./index.js",
 
-export default {
-  context: __dirname,
-  entry: {
-    'bundle.js': './index.jsx',
-    'bundle.css': './assets/less/main.less'    
-  },
   output: {
-    path: `${__dirname}/build/`,
-    filename: '[name]'
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/",
+    filename: "bundle.js"
   },
+
+  resolve: {
+    extensions: [".jsx", ".js", ".json", ".less"],
+    modules: [
+      path.resolve(__dirname, "src/lib"),
+      path.resolve(__dirname, "node_modules"),
+      "node_modules"
+    ],
+    alias: {
+      components: path.resolve(__dirname, "src/components"), // used for tests
+      style: path.resolve(__dirname, "src/style"),
+      react: "preact-compat",
+      "react-dom": "preact-compat"
+    }
+  },
+
   module: {
-    loaders: [
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: path.resolve(__dirname, "src"),
+        enforce: "pre",
+        use: "source-map-loader"
+      },
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
-      },{
-        test: /\.less$/,
+        use: "babel-loader"
+      },
+      {
+        // Transform our own .(less|css) files with PostCSS and CSS-modules
+        test: /\.(less|css)$/,
+        include: [path.resolve(__dirname, "src/components")],
         use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'less-loader']
+          fallback: "style-loader",
+          use: [
+            {
+              loader: "css-loader",
+              options: {
+                modules: true,
+                sourceMap: CSS_MAPS,
+                importLoaders: 1,
+                minimize: true
+              }
+            },
+            {
+              loader: `postcss-loader`,
+              options: {
+                sourceMap: CSS_MAPS,
+                plugins: () => {
+                  autoprefixer({ browsers: ["last 2 versions"] });
+                }
+              }
+            },
+            {
+              loader: "less-loader",
+              options: { sourceMap: CSS_MAPS }
+            }
+          ]
         })
+      },
+      {
+        test: /\.(less|css)$/,
+        exclude: [path.resolve(__dirname, "src/components")],
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: [
+            {
+              loader: "css-loader",
+              options: { sourceMap: CSS_MAPS, importLoaders: 1, minimize: true }
+            },
+            {
+              loader: `postcss-loader`,
+              options: {
+                sourceMap: CSS_MAPS,
+                plugins: () => {
+                  autoprefixer({ browsers: ["last 2 versions"] });
+                }
+              }
+            },
+            {
+              loader: "less-loader",
+              options: { sourceMap: CSS_MAPS }
+            }
+          ]
+        })
+      },
+      {
+        test: /\.json$/,
+        use: "json-loader"
+      },
+      {
+        test: /\.(xml|html|txt|md)$/,
+        use: "raw-loader"
+      },
+      {
+        test: /\.(svg|woff2?|ttf|eot|jpe?g|png|gif)(\?.*)?$/i,
+        use: ENV === "production" ? "file-loader" : "url-loader"
       }
     ]
   },
-  resolve: {
-    extensions: ['.js', '.jsx', '.css']
+  plugins: [
+    new webpack.NoEmitOnErrorsPlugin(),
+    new ExtractTextPlugin({
+      filename: "style.css",
+      allChunks: true,
+      disable: ENV !== "production"
+    }),
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify(ENV)
+    }),
+    new HtmlWebpackPlugin({
+      template: "./index.ejs",
+      minify: { collapseWhitespace: true }
+    }),
+    new CopyWebpackPlugin([
+      { from: "./manifest.json", to: "./" },
+      { from: "./favicon.ico", to: "./" }
+    ])
+  ].concat(
+    ENV === "production"
+      ? [
+          new webpack.optimize.UglifyJsPlugin({
+            output: {
+              comments: false
+            },
+            compress: {
+              unsafe_comps: true,
+              properties: true,
+              keep_fargs: false,
+              pure_getters: true,
+              collapse_vars: true,
+              unsafe: true,
+              warnings: false,
+              screw_ie8: true,
+              sequences: true,
+              dead_code: true,
+              drop_debugger: true,
+              comparisons: true,
+              conditionals: true,
+              evaluate: true,
+              booleans: true,
+              loops: true,
+              unused: true,
+              hoist_funs: true,
+              if_return: true,
+              join_vars: true,
+              cascade: true,
+              drop_console: true
+            }
+          }),
+
+          new OfflinePlugin({
+            relativePaths: false,
+            AppCache: false,
+            excludes: ["_redirects"],
+            ServiceWorker: {
+              events: true
+            },
+            cacheMaps: [
+              {
+                match: /.*/,
+                to: "/",
+                requestTypes: ["navigate"]
+              }
+            ],
+            publicPath: "/"
+          })
+        ]
+      : []
+  ),
+
+  stats: { colors: true },
+
+  node: {
+    global: true,
+    process: false,
+    Buffer: false,
+    __filename: false,
+    __dirname: false,
+    setImmediate: false
   },
-  plugins,
+
+  devtool: ENV === "production" ? "source-map" : "cheap-module-eval-source-map",
+
   devServer: {
-    port: 3021,
-    hot: true,
-    historyApiFallback: {
-      index: './index.html'
+    port: process.env.PORT || 8080,
+    host: "localhost",
+    publicPath: "/",
+    contentBase: "./src",
+    historyApiFallback: true,
+    open: true,
+    openPage: "",
+    proxy: {
+      // OPTIONAL: proxy configuration:
+      // '/optional-prefix/**': { // path pattern to rewrite
+      //   target: 'http://target-host.com',
+      //   pathRewrite: path => path.replace(/^\/[^\/]+\//, '')   // strip first path segment
+      // }
     }
   }
-}
+};
