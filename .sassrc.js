b23fefa1c5343d1a4375d4d079d21e5bb47348e4
Oliver.W <ole3021@gmail.com>
2018-06-12 21:36:31 +0800

    fix the sass build issue

--- .sassrc.js
+++ .sassrc.js
@@ -1,12 +1,6 @@
-// const path = require('path')
-// const cwd = process.cwd()
+const path = require('path')
+const cwd = process.cwd()
 
-// module.exports = {
-//   data: '@import "globals";',
-//   includePaths: [
-//     path.resolve(cwd, 'node_modules/susy/sass'),
-//     path.resolve(cwd, 'node_modules/normalize.scss'),
-//     path.resolve(cwd, 'node_modules'),
-//     path.resolve(cwd, 'static', 'scss')
-//   ]
-// }
+module.exports = {
+  includePaths: [path.resolve(cwd, 'node_modules'), path.resolve(cwd, 'src')]
+}
