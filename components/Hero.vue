981e3651a732cc3581ec7a7fe39621f44d245f0f
Oliver.W <ole3021@gmail.com>
2019-06-17 12:51:24 +0800

    fix: header style

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,10 +1,10 @@
 <template>
-  <section class="hero is-primary">
-    <img class="background" src="images/austria.jpg" alt="background">
+  <section class="hero is-medium is-primary has-background">
+    <img class="hero-background is-transparent" src="images/austria.jpg" alt="background">
     <div class="hero-body">
       <div class="container">
-        <aside class="side">
-          <figure class="image">
+        <aside class="avatar">
+          <figure class="image is-256x256">
             <img
               class="is-rounded"
               src="images/alex.jpg"
@@ -20,17 +20,12 @@
           >I'm a Backend Developer, Architect, Scrum Master, and Open-Source Contributor</p>
           <hr class="hr">
           <div class="description">
-            <p>I do Node.js, Flutter, Nest.js, Loopback, MongoDB, Rethink DB, etc</p>
+            <p>I use TypeScript, Node.js, GraphQL, Nest.js, Flutter, MongoDB, PostgreSQL, etc</p>
           </div>
           <div class="contact">
-            <a class="button is-dark" href="mailto: ole3021@gmail.com">Get in touch</a>
+            <a class="button is-light" href="mailto: ole3021@gmail.com">Get in touch</a>
           </div>
           <ul class="social">
-            <li>
-              <a href="https://twitter.com/ole3021" target="_blank">
-                <i class="fab fa-twitter"/>
-              </a>
-            </li>
             <li>
               <a href="https://github.com/ole3021" target="_blank">
                 <i class="fab fa-github"/>
@@ -39,6 +34,11 @@
             <li>
               <a href="https://www.linkedin.com/in/ole3021/" target="_blank">
                 <i class="fab fa-linkedin"/>
+              </a>
+            </li>
+            <li>
+              <a href="https://twitter.com/ole3021" target="_blank">
+                <i class="fab fa-twitter"/>
               </a>
             </li>
           </ul>
@@ -51,91 +51,74 @@
 <style lang="sass">
 @import "../node_modules/bulma/sass/utilities/_all.sass"
 
-.hero 
-  position: relative
-  overflow: hidden
+.hero
+  &.has-background
+    position: relative
+    overflow: hidden
+  &-background
+    position: absolute
+    object-fit: cover
+    object-position: center center
+    width: 100%
+    height: 100%
+    &.is-transparent
+      opacity: 0.3
 
-.background
-  position: absolute
-  object-fit: cover
-  object-position: center center
-  width: 100%
-  height: 100%
-  opacity: 0.2
 
 .container
   display: flex
-  position: relative
-  min-height: 40vh
-  // min-height: 70vh
   justify-content: space-around
   justify-content: space-evenly
   align-items: center
   +touch
     flex-direction: column
-
   +desktop
     justify-content: space-around
     justify-content: space-evenly
-    
-.about
-  max-width: 37rem    
 
-.side
+.avatar
   +touch
-    max-height: 15rem
-    max-width: 15rem
     margin-bottom: 2rem
-
   +desktop
-    max-height: 20rem
-    max-width: 20rem
     order: 2
 
-.name 
-  font-size: 2.25rem
-  line-height: 1.125
-  margin-bottom: 0.5rem
 
-.hr 
-  background-color: #ff470f
-  border: none
-  content: ''
-  height: 4px
-  margin-bottom: 1.5rem
-  margin-top: 1.5rem
-  transform-origin: center left
-  width: 4rem
-
-.job,
-  font-family: 'Monsterrat', 'Quicksand', sans-serif
-  letter-spacing: 0.3rem
-  text-transform: uppercase
-
-.contact
-  margin-top: 1rem
-
-.social
-  display: flex
-  margin-top: 1rem
-  
-  li
-    margin-right: 0.5rem
-    text-align: center
-    height: 2rem
-    width: 2rem
-
-  a
-    align-items: center
-    font-size: 1.7rem
-    opacity: 0.5
-    will-change: opacity, transform
-
-  a:hover
-    opacity: 1
-    // transform: scale(1.5) FIXME: not working
-
-  a:active 
-    opacity: 1
-    // transform: scale(1) FIXME: not working
+.about
+  .name
+    font-size: 2.25rem
+    line-height: 1.125
+    margin-bottom: 0.5rem
+  .job,
+    font-family: 'Monsterrat', 'Quicksand', sans-serif
+    letter-spacing: 0.3rem
+    text-transform: uppercase
+  .hr
+    background-color: #ff470f
+    border: none
+    content: ''
+    height: 4px
+    margin: 1rem 0
+    transform-origin: center left
+    width: 4rem
+  .contact
+    margin-top: 1rem
+  .social
+    display: flex
+    margin-top: 1rem
+    li
+      margin-right: 0.7rem
+      text-align: center
+      height: 2rem
+      width: 2rem
+    a
+      align-items: center
+      font-size: 1.9rem
+      opacity: 0.5
+      will-change: opacity, transform
+      transition-duration: 0.3s
+    a:hover
+      opacity: 1
+      transition-duration: 0.3s
+    a:active
+      opacity: 1
 </style>
